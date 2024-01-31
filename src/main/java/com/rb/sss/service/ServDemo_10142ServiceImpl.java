fe4019832331583335220d868c2869fbb46734a6
Developer Platform <>
2024-01-31 17:58:32 +0200

    Init Upload

---
+++ src/main/java/com/rb/sss/service/ServDemo_10142ServiceImpl.java
@@ -0,0 +1,26 @@
+package com.rb.sss.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+ 
+   
+  import com.rb.sss.model.EmployeeRes; 
+   
+   
+      
+  import com.rb.sss.model.EmployeeReq; 
+
+
+
+@ApplicationScoped
+public class ServDemo_10142ServiceImpl implements ServDemo_10142Service{
+   
+     @Override  
+      public  EmployeeRes getEmployee 
+      (EmployeeReq  request){
+
+        
+          return null;
+          
+      }
+    
+}  
---
+++ src/test/java/com/rb/sss/controller/ServDemo_10142TestController.java
@@ -0,0 +1,9 @@
+package com.rb.sss.controller;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ServDemo_10142TestController {
+
+
+
+}    
